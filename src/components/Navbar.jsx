202d9afa53690694837267b15ca2e58a6e91619c
Abraham Castillo <adcdabrahamcastillo@gmail.com>
2024-04-02 13:38:40 +0000

    Landing lista, con responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@
 /*import { React, useState, useEffect } from "react";
  import { Link } from "react-router-dom";*/
 
-import unimetLogo from '/logo.png'
+import unimetLogo from '/logo.svg'
 
 const Navbar = () => {
     return (
