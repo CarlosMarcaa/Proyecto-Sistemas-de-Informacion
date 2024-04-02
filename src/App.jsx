81bb42cabaacf7743a459f81047e48f5aee3cd13
Abraham Castillo <adcdabrahamcastillo@gmail.com>
2024-04-02 14:44:20 +0000

    Faltan detalles del login

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Landing from '../src/components/Landing'
 
+import Login from '../src/components/Login'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <Landing/>
+      {/*<Login/>*/}
     </>
   )
 }
