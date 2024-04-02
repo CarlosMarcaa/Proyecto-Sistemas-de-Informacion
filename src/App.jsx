fee8eb163fe524ec0e69ffb6b1c73a2a1d55f3c6
Grimidk <thegrimidk@gmail.com>
2024-04-02 11:14:28 -0400

    route

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,36 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Landing from '../src/components/Landing'
-
 import Login from '../src/components/Login'
+
 function App() {
 
   return (
     <>
-      <Landing/>
-      {/*<Login/>*/}
+      <Router>
+        <Routes>
+          <Route 
+          exact path='/'
+          element={<Landing />}
+          />
+          <Route 
+          exact path='/login'
+          element={<Login />}
+          />
+          {/* <Route 
+          exact path='/register'
+          element={<Register />}
+          />
+          <Route 
+          exact path='/groups'
+          element={<Groups />}
+          />
+          <Route 
+          exact path='/sports'
+          element={<Sports />}
+          /> */}
+        </Routes>
+      </Router>
     </>
   )
 }
