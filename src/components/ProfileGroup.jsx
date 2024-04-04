552dddcd437e5febd37534a7049eedf9efe5d81d
Abraham Castillo <adcdabrahamcastillo@gmail.com>
2024-04-04 13:14:29 +0000

    responsive de ProfileGroup Listo

--- src/components/ProfileGroup.jsx
+++ src/components/ProfileGroup.jsx
@@ -6,9 +6,9 @@ const ProfileGroup = () => {
         <>
             <Navbar/>
             <HeaderSmall/>
-            <div className="container">
-                <div className="row gx-4 gx-lg-5 align-items-center my-5">
-                    <div className="col-lg-6">
+            <div className="container profile-group">
+                <div className="row gx-4 gx-lg-5 my-5">
+                    <div className="col-lg-6 img-profile">
                         <img
                         className="img-fluid rounded mb-4 mb-lg-0"
                         src="https://dummyimage.com/500x400/dee2e6/6c757d.jpg"
@@ -16,7 +16,7 @@ const ProfileGroup = () => {
                         />
                     </div>
                     <div className="col-lg-6">
-                        <span>Categoria / Nombre del grupo</span>
+                        <span className="ruta-grupo">Categoria / Nombre del grupo</span>
                         <h2 className="font-weight-light">Nombre Agrupacion</h2>
                         <div className="d-flex small text-warning mb-2">
                             <div className="bi-star-fill"></div>
@@ -30,37 +30,35 @@ const ProfileGroup = () => {
                         much fancy flare to it, but it makes a great use of the standard Bootstrap
                         core components. Feel free to use this template for any project you want!
                         </p>
-                        <a className="btn btn-primary" href="#!">
-                        Call to Action!
-                        </a>
+                        <a href="#" className="btn btn-profile-group">Ver muro</a>
                     </div>
                     <div className="col-lg-6">
-                        <div className="title-collapse">
-                            <a className="" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                        <a className="" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                            <div className="title-collapse">
                                 Mision
                                 <i className="bi bi-chevron-down"></i>
-                            </a>
-                        </div>
+                            </div>
+                        </a>
                         <div className="collapse" id="collapseExample">
                             Some placeholder content for the collapse component. This panel is hidden
                             by default but revealed when the user activates the relevant trigger.
                         </div>
-                        <div className="title-collapse">
-                            <a className="" data-bs-toggle="collapse" href="#collapseExample2" role="button" aria-expanded="false" aria-controls="collapseExample">
+                        <a className="" data-bs-toggle="collapse" href="#collapseExample2" role="button" aria-expanded="false" aria-controls="collapseExample">
+                            <div className="title-collapse">
                                 Visión
                                 <i className="bi bi-chevron-down"></i>
-                            </a>
-                        </div>
+                            </div>
+                        </a>
                         <div className="collapse" id="collapseExample2">
                             Some placeholder content for the collapse component. This panel is hidden
                             by default but revealed when the user activates the relevant trigger.
                         </div>
-                        <div className="title-collapse">
-                            <a className="" data-bs-toggle="collapse" href="#collapseExample3" role="button" aria-expanded="false" aria-controls="collapseExample">
+                        <a className="" data-bs-toggle="collapse" href="#collapseExample3" role="button" aria-expanded="false" aria-controls="collapseExample">
+                            <div className="title-collapse">
                                 Objetivos
                                 <i className="bi bi-chevron-down"></i>
-                            </a>
-                        </div>
+                            </div>
+                        </a>
                         <div className="collapse" id="collapseExample3">
                             Some placeholder content for the collapse component. This panel is hidden
                             by default but revealed when the user activates the relevant trigger.
@@ -71,58 +69,74 @@ const ProfileGroup = () => {
                         <table className="table">
                             <thead>
                                 <tr>
-                                <th scope="col">First</th>
-                                <th scope="col">Last</th>
+                                <th scope="col">Caracteristica</th>
+                                <th scope="col">Valor</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
-                                <td>Mark</td>
-                                <td>Otto</td>
+                                    <td>Categoría</td>
+                                    <td>Otto</td>
                                 </tr>
                                 <tr>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
+                                    <td>Facultad</td>
+                                    <td>Thornton</td>
                                 </tr>
                                 <tr>
-                                <td>Thornton</td>
-                                <td colSpan={2}>Larry the Bird</td>
+                                    <td>Año de Creacion</td>
+                                    <td>Larry the Bird</td>
+                                </tr>
+                                <tr>
+                                    <td>Responsable</td>
+                                    <td>Thornton</td>
+                                </tr>
+                                <tr>
+                                    <td>Ubicacion</td>
+                                    <td>Larry the Bird</td>
                                 </tr>
                             </tbody>
                         </table>
 
                     </div>
                     <div className="col-lg-6">
-                        <h3>Informacion extra</h3>
-                        <div className="col-lg-4">
-                            <h1>5.0</h1>
-                        </div>
-                        <div className="col-lg-8">
-                            <div className="d-flex small text-warning mb-2">
-                                <div className="bi-star-fill"></div>
-                                <div className="bi-star-fill"></div>
-                                <div className="bi-star-fill"></div>
-                                <div className="bi-star-fill"></div>
-                                <div className="bi-star-fill"></div>
+                        <h3>Opiniones</h3>
+                        <div className="row puntuacion-general">
+
+                        
+                            <div className="col-lg-2 puntuacion-total">
+                                <h1>5.0</h1>
                             </div>
-                            <h3>(1 calificacion)</h3>
-                        </div>
-                        <div>
-                            <div className="d-flex small text-warning mb-2">
-                                <div className="bi-star-fill"></div>
-                                <div className="bi-star-fill"></div>
-                                <div className="bi-star-fill"></div>
-                                <div className="bi-star-fill"></div>
-                                <div className="bi-star-fill"></div>
+                            <div className="col-lg-10 puntuacion-estrellas">
+                                <div className="d-flex small text-warning">
+                                    <div className="bi-star-fill"></div>
+                                    <div className="bi-star-fill"></div>
+                                    <div className="bi-star-fill"></div>
+                                    <div className="bi-star-fill"></div>
+                                    <div className="bi-star-fill"></div>
+                                </div>
+                                <h5>(1 calificacion)</h5>
                             </div>
-                            <span>09 jul. 2021</span>
+                        </div>
+                        <div className="row top-comentario">
+                            <div className="col-lg-6 col-md-6">
+                                <div className="d-flex small text-warning mb-2">
+                                    <div className="bi-star-fill"></div>
+                                    <div className="bi-star-fill"></div>
+                                    <div className="bi-star-fill"></div>
+                                    <div className="bi-star-fill"></div>
+                                    <div className="bi-star-fill"></div>
+                                </div>
+                            </div>
+                            <div className="col-lg-6 col-md-6 fecha">
+                                <span>09 jul. 2021</span>
+                            </div>
                             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                                  Voluptatem, tempore nesciunt beatae molestiae rerum 
                                  dolore omnis dignissimos cupiditate in animi suscipit
                                   sapiente sint ipsam.</p>
                         </div>
                     </div>
-                    <div className="col-lg-6">
+                    <div className="col-lg-6 comentar">
                         <h3>Comparte tu opinion</h3>
                         
                         <div className="d-flex small text-warning mb-2">
@@ -143,7 +157,7 @@ const ProfileGroup = () => {
                             />
                             <label htmlFor="floatingTextarea2">Tu opinión</label>
                         </div>
-                        <a href="#" className="btn btn-login">Publicar</a>
+                        <a href="#" className="btn btn-profile-group">Publicar</a>
                     </div>
                 </div>
             </div>
